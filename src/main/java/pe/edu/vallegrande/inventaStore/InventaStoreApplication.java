e6b603fa0e4ef019a54269d3c27b6c6fbdd1ca72
PaulHuayhua <paul.huayhua.jaquehua@vallegrande.edu.pe>
2025-04-10 01:21:20 +0000

    feat: initialize Spring Boot project structure

---
+++ src/main/java/pe/edu/vallegrande/inventaStore/InventaStoreApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.inventaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaStoreApplication.class, args);
+	}
+
+}
